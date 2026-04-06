ad5c5fb224b99f845ab73625036e9ccd9b090805
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2026-04-06 19:09:52 +1200

    Center main text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { StartButton } from "@/src/components/StartButton";
 const Home = () => {
   return (
     <main className="flex flex-col items-center justify-center h-full gap-6">
-      <h1 className="text-5xl font-bold">Guess That Deck!</h1>
+      <h1 className="text-5xl font-bold text-center">Guess That Deck!</h1>
       <StartButton />
     </main>
   );
